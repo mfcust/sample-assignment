d4079dcd1200bed4097674439f510219f81d7d20
mfcust <43455391+mfcust@users.noreply.github.com>
2020-11-30 12:21:51 -0800

    Create sample-assignment.py

---
+++ sample-assignment.py
@@ -0,0 +1,8 @@
+# 1) Print 'Hello World' to the console by copying and pasting the code from the markdown file
+
+
+
+
+
+
+# 2) Commit and push your repl by going to version control in the top left of your repl. Don't forget to type in a message to the text box before you commit and push!
